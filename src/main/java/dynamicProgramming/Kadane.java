5276145e34fd84a353b6003fd10d4084bfafb93c
Prathamesh Kawale <kawa5@live.com>
2017-01-29 12:16:14 +0530

    modified Kadane algo

--- src/main/java/dynamicProgramming/Kadane.java
+++ src/main/java/dynamicProgramming/Kadane.java
@@ -2,13 +2,25 @@ package dynamicProgramming;
 
 public class Kadane {
 
-	public static void main(String[] args) {
-		int[] numbers = { -2, -1, 0, 3, -4, -5 };
-		findMaxSubArray(numbers);
-
+	// Works only if there is atleast one positive element
+	public static int kadane(int[] numbers) {
+		int max_end_here = 0;
+		int max_so_far = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			max_end_here += numbers[i];
+			if (max_end_here < 0) {
+				max_end_here = 0;
+			}
+			if (max_so_far < max_end_here) {
+				max_so_far = max_end_here;
+			}
+		}
+		System.out.println(max_so_far);
+		return max_so_far;
 	}
 
-	public static void findMaxSubArray(int[] numbers) {
+	// For all negative elements in an array
+	public static void kadaneModified(int[] numbers) {
 		int max_so_far = 0;
 		int max_ending_here = 0;
 		for (int x : numbers) {
@@ -16,5 +28,12 @@ public class Kadane {
 			max_so_far = Math.max(max_so_far, max_ending_here);
 		}
 		System.out.println(max_so_far);
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = { -2, -1, 3, 4, -5 };
+		kadaneModified(numbers);
+		kadane(numbers);
+
 	}
 }
