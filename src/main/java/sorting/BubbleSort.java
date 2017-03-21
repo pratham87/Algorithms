9d21db84bb2d0fad98bab759b5ee834d399f416b
Prathamesh Kawale <kawa5@live.com>
2017-03-21 01:13:32 -0700

    BubbleSort notes

--- src/main/java/sorting/BubbleSort.java
+++ src/main/java/sorting/BubbleSort.java
@@ -1,6 +1,43 @@
 package sorting;
 
+/*
+ Time Complexity: O(n^2)
+ Space Complexity: O(1)
+
+ Algorithm:
+ 
+ 1. Compare array[0] & array[1] 
+ 2. If array[0] > array [1] swap it. 
+ 3. Compare array[1] & array[2] 
+ 4. If array[1] > array[2] swap it. ... 
+ 5. Compare array[n-1] & array[n] 
+ 6. if [n-1] > array[n] then swap it.
+ 
+ 7. After this step we will have largest element at the last index.
+ 
+ 8. Repeat the same steps for array[1] to array[n-1]
+ 
+ */
 public class BubbleSort {
+
+	private static void bubbleSort(int[] intArray) {
+
+		int n = intArray.length;
+		int temp = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < (n - i); j++) {
+
+				if (intArray[j - 1] > intArray[j]) {
+					temp = intArray[j - 1];
+					intArray[j - 1] = intArray[j];
+					intArray[j] = temp;
+				}
+
+			}
+		}
+
+	}
 
 	public static void main(String[] args) {
 
@@ -26,44 +63,4 @@ public class BubbleSort {
 
 	}
 
-	private static void bubbleSort(int[] intArray) {
-
-		int n = intArray.length;
-		int temp = 0;
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 1; j < (n - i); j++) {
-
-				if (intArray[j - 1] > intArray[j]) {
-					temp = intArray[j - 1];
-					intArray[j - 1] = intArray[j];
-					intArray[j] = temp;
-				}
-
-			}
-		}
-
-	}
-
 }
-
-/**
- * Working
- * 
- * 
- * In bubble sort, we basically traverse the array from first to array_length -
- * 1 position and compare the element with the next one. Element is swapped with
- * the next element if the next element is greater.
- * 
- * Bubble sort steps are as follows.
- * 
- * 1. Compare array[0] & array[1] 2. If array[0] > array [1] swap it. 3. Compare
- * array[1] & array[2] 4. If array[1] > array[2] swap it. ... 5. Compare
- * array[n-1] & array[n] 6. if [n-1] > array[n] then swap it.
- * 
- * After this step we will have largest element at the last index.
- * 
- * Repeat the same steps for array[1] to array[n-1]
- * 
- * 
- */
