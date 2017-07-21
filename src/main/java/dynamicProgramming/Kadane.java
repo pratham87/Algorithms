8c51c650290484cdcbae27ab1ae9b44cd5c24c82
Prathamesh Kawale <kawa5@live.com>
2017-07-20 21:51:57 -0700

    Update Kadane.java

--- src/main/java/dynamicProgramming/Kadane.java
+++ src/main/java/dynamicProgramming/Kadane.java
@@ -35,7 +35,7 @@ public class Kadane {
 
 	public static void main(String[] args) {
 		int[] numbers = { -2, -1, 3, 4, -5 };
-		kadaneModified(numbers);
+		kadane(numbers);
 		maxSubArray(numbers);
 
 	}
