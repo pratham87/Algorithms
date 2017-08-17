32d3193c97f6328167cea8468d803251aa7d0f7c
Prathamesh Kawale <kawa5@live.com>
2017-08-16 18:53:46 -0700

    LongestPalindromicSubsequence

--- src/main/java/dynamicProgramming/LongestPalindromicSubsequence.java
+++ src/main/java/dynamicProgramming/LongestPalindromicSubsequence.java
@@ -3,37 +3,31 @@ package dynamicProgramming;
 public class LongestPalindromicSubsequence {
 
 	public static int lps(String s) {
-		int n = s.length();
-		int[][] L = new int[n][n];
+		int[][] demo = new int[s.length()][s.length()];
 
-		for (int i = 0; i < n; i++) {
-			L[i][i] = 1;
+		for (int i = 0; i < s.length(); i++) {
+			demo[i][i] = 1;
 		}
 
-		for (int cl = 2; cl <= n; cl++) {
-			for (int i = 0; i < n - cl + 1; i++) {
-				int j = i + cl - 1;
-				if (s.charAt(i) == s.charAt(j) && cl == 2) {
-					L[i][j] = 2;
+		for (int l = 2; l <= s.length(); l++) {
+			for (int i = 0; i < s.length() - l + 1; i++) {
+				int j = i + l - 1;
+				if (l == 2 && s.charAt(i) == s.charAt(j)) {
+					demo[i][j] = 2;
+				} else if (s.charAt(i) == s.charAt(j)) {
+					demo[i][j] = 2 + demo[i + 1][j - 1];
+				} else {
+					demo[i][j] = Math.max(demo[i + 1][j], demo[i][j - 1]);
 				}
-
-				else if (s.charAt(i) == s.charAt(j)) {
-					L[i][j] = L[i + 1][j - 1] + 2;
-				}
-
-				else {
-					L[i][j] = Math.max(L[i][j - 1], L[i + 1][j]);
-				}
-
 			}
 		}
 
-		return L[0][n - 1];
+		return demo[0][s.length() - 1];
 	}
 
 	public static void main(String[] args) {
-		String seq = "agbdba";
-		System.out.println("The length of the lps is " + lps(seq));
+		String s = "agbdba";
+		System.out.println("The length of the lps is " + lps(s));
 	}
 
 }
